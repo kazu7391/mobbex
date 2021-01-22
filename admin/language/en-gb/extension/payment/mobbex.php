b15230b9f0916bafb373a681295cf3d227325c66
Kazu <vinhtq.0703@gmail.com>
2021-01-22 18:46:59 +0700

    Mobbex module

--- admin/language/en-gb/extension/payment/mobbex.php
+++ admin/language/en-gb/extension/payment/mobbex.php
@@ -1,1 +1,19 @@
 <?php
+// Heading
+$_['heading_title']      = 'Mobbex Payment Gateway';
+
+// Text
+$_['text_extension']     = 'Extensions';
+$_['text_success']       = 'Success: You have modified Mobbex payment module!';
+$_['text_edit']          = 'Edit Mobbex Payment';
+
+// Entry
+$_['entry_order_status'] = 'Order Status';
+$_['entry_status']       = 'Status';
+$_['entry_sort_order']   = 'Sort Order';
+
+// Help
+$_['help_total']         = 'The checkout total the order must reach before this payment method becomes active.';
+
+// Error
+$_['error_permission']   = 'Warning: You do not have permission to modify payment Mobbex!';
